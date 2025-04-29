93476153ae0b72a64d1016f34cd5dc6c75f62742
krob79 <emailkyleroberts@gmail.com>
2025-04-29 12:36:00 -0400

    too many changes to name - very irresponsible commit, but it's just a tutorial

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+// export const experimental_ppr = true; //used only in experimental version of NextJS
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
-    ],
-  },
+  // experimental:{
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
